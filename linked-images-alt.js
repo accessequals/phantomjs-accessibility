b64153263f1235f0e10d6249dd0a7dee47befb19
William Gates <accessequals@gmail.com>
2015-02-12 10:08:25 +0000

    linked-images-alt.js logs alt text of linked images

--- linked-images-alt.js
+++ linked-images-alt.js
@@ -12,27 +12,17 @@ page.open( url, function( status ) {
 		/*
 		* test paragraphs which might be acting as headings
 		*/
-		var suspectHeadings = page.evaluate( function() {
+		var linkedImages = page.evaluate( function() {
 
-			function getStyle( element, cssProperty ) {
+			var linkedImg = document.querySelectorAll( 'a img' );
 
-				return window.getComputedStyle( element, null ).getPropertyCSSValue( cssProperty ).cssText;
-			}
+			var linkedImageAltText = [];
 
-			var paras = document.querySelectorAll('p');
+			for( var i = 0; i < linkedImg.length; i++ ){
+      			linkedImageAltText.push( linkedImg[ i ].getAttribute( 'alt' ) );
+    		}
 
-			var shortBold = 0;
-
-			for ( var i = 0; i < paras.length; i++ ) {
-
-				// check that paragraph length < 50 and paragraph paragraph css is 'bold'
-				if ( paras[ i ].innerHTML.length < 50 && getStyle( paras[ i ], 'font-weight' ) === 'bold'  ) {
-
-					shortBold = shortBold + 1;
-				}
-			}
-
-			return shortBold;
+			return linkedImageAltText;
 
 		});
 
@@ -43,10 +33,13 @@ page.open( url, function( status ) {
 	*/
 
 	// first suspect headings
-	if ( suspectHeadings > 0 ) {
-		console.log('Warning WCAG2 Level A, 1.3.1 - page contains short, bold paragraphs which might be acting as headings');
+	if ( linkedImages.length > 0 ) {
+		console.log( 'There are linked images with alt attributes' );
+		for( var j = 0; j < linkedImages.length; j++ ) {
+		    console.log( ( j + 1 ) + ' | ' + linkedImages[ j ] );
+		 }
 	} else {
-		console.log('WCAG2 Level A, 1.3.1 - page contains no short, bold paragraphs which might be acting as headings');
+		console.log( 'There are no linked images' );
 	}
 
 
